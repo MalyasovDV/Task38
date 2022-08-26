1d34f7418ab552b114c649c27c8481d6ddd58cd9
Dmitry_Malyasov <dmalyasov24@gmail.com>
2022-08-26 16:22:26 +0800

    Task done

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿void Output(float[] array)
+{
+    for (int i = 0; i < array.Length; ++i)
+        Console.WriteLine(array[i]);
+}
+
+float DiffBetweenMaxAndMin(float[] array)
+{
+    float difference, max = array[0], min = array[0];
+
+    for (int i = 1; i < array.Length; ++i)
+    {    
+        if (array[i] > max)
+            max = array[i];
+        else
+            if (min > array[i])
+                min = array[i];
+    }
+
+    Console.WriteLine(max);
+    Console.WriteLine(min);
+
+    difference = max - min;
+
+    return difference;
+}
+
+Random rnd = new Random();
+float[] array = new float[5];
+
+for (int i = 0; i < array.Length; ++i)
+    array[i] = rnd.Next(-100,100);
+
+Output(array);
+Console.WriteLine();
+Console.WriteLine(DiffBetweenMaxAndMin(array));
